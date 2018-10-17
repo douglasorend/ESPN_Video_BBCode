7a6cbae36ffc94e7c05231c68aa910796475d902
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-17 11:31:53 -0500

    v1.2 - October 2nd, 2015
    v1.2 - October 2nd, 2015
    o Modified validation function to check to make sure URL is actually a ESPN video URL.
    o Added "Invalid ESPN URL" message for use with invalid ESPN URLs (duh)....
    o Added ability to auto-embed ESPN URLs to the mod.
    o Added code to remove auto-embedded ESPN videos from within code tags.

--- Subs-BBCode-ESPN.php
+++ Subs-BBCode-ESPN.php
@@ -50,12 +50,24 @@ function BBCode_ESPN_Button(&$buttons)
 
 function BBCode_ESPN_Validate(&$tag, &$data, &$disabled)
 {
+	global $txt;
+	
 	if (empty($data))
-		return ($tag['content'] = '');
-	list($width, $height) = explode('|', $tag['content']);
-	parse_str(parse_url(str_replace('&amp;', '&', $data), PHP_URL_QUERY), $out);
-	$data = (isset($out['id']) ? $out['id'] : ((isset($out['externalId']) ? str_replace('espn:', '', $out['externalId']) : (int) $data)));
-	$tag['content'] = (empty($data) ? '' : '<script src="http://player.espn.com/player.js?playerBrandingId=4ef8000cbaf34c1687a7d9a26fe0e89e&adSetCode=91cDU6NuXTGKz3OdjOxFdAgJVtQcKJnI&pcode=1kNG061cgaoolOncv54OAO1ceO-I&width=' . $width .' &height=' . $height . '&externalId=espn:' . $data . '&thruParam_espn-ui[autoPlay]=false&thruParam_espn-ui[playRelatedExternally]=true"></script>');
+		return ($tag['content'] = $txt['espn_invalid']);
+	list($width, $height, $id) = explode('|', $tag['content'] . '|' . ((int) $data));
+	if (preg_match('#(http|https):\/\/espn\.go.com/video/clip\?id=(|espn:)(\d+)#i', $data, $parts))
+		$id = $parts[3];
+	$tag['content'] = (empty($id) ? $txt['espn_invalid'] : '<script src="http://player.espn.com/player.js?playerBrandingId=4ef8000cbaf34c1687a7d9a26fe0e89e&adSetCode=91cDU6NuXTGKz3OdjOxFdAgJVtQcKJnI&pcode=1kNG061cgaoolOncv54OAO1ceO-I&width=' . $width .'&height=' . $height . '&externalId=espn:' . $id . '&thruParam_espn-ui[autoPlay]=false&thruParam_espn-ui[playRelatedExternally]=true"></script>');
+}
+
+function BBCode_ESPN_Embed(&$message)
+{
+	$pattern = '#(|\[espn(|.+?)\](([<br />]+)?))(http|https):\/\/(video\.espn\.com|espn\.go.com)/video/clip\?id=(|espn:)(\d+)(([<br />]+)?)(\[/espn\]|)#i';
+	$message = preg_replace($pattern, '[espn$2]$5://$6/video/clip?id=$8[/espn]', $message);
+	$pattern = '#(|\[espn(|.+?)\](([&lt;br /&gt;]+)?))&lt;script src=&quot;(http|https):\/\/player\.espn\.com/player\.js\?(.+?&amp;|)externalId=espn:(\d+)(|&amp;.+?)&quot;&gt;&lt;/script&gt;(([&lt;br /&gt;]+)?)(\[/espn\]|)#i';
+	$message = preg_replace($pattern, '[espn$2]$5://espn.go.com/video/clip?id=$7[/espn]', $message);
+	$pattern = '#\[code(|(.+?))\](|.+?)\[espn(|.+?)\](.+?)\[/espn\](|.+?)\[/code\]#i';
+	$message = preg_replace($pattern, '[code$1]$3$5$6[/code]', $message);
 }
 
 ?>
